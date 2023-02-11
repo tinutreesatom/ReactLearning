dda128fd55bd77bb3435e7d3083584768ac0a2ea
Tinu Treesa Tom <tinutreesatom@gmail.com>
2023-02-11 02:07:35 +0000

    Update App.js and UseffectEg.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Emotion from "./Emotion";
 import "./styles.css";
+import UseEffectEg from "./UseEffectEg";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <h2>Here you can set your emotion! </h2>
       <i>Click the button to set your emotion sad</i>
       <Emotion />
+      <UseEffectEg />
     </div>
   );
 }
---
+++ src/UseffectEg.js
@@ -0,0 +1,7 @@
+
+import { react } from "react";
+import { UseState, UseEffect } from "react";
+function UseEffectEg() {
+  return <div>hello hi</div>;
+}
+export default UseEffectEg;
